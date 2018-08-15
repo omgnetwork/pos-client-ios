3e5bdba3cf63f5f0849f0a2feb961ee1fffe4073
mederic <mederic@omise.co>
2018-08-15 11:14:38 +0700

    Initial Commit

---
+++ POS-client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POS-client
+//
+//  Created by Mederic Petit on 15/8/18.
+//  Copyright © 2018 Omise Go Pte. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ POS-clientTests/POS_clientTests.swift
@@ -0,0 +1,36 @@
+//
+//  POS_clientTests.swift
+//  POS-clientTests
+//
+//  Created by Mederic Petit on 15/8/18.
+//  Copyright © 2018 Omise Go Pte. Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import POS_client
+
+class POS_clientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
