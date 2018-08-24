eda5f22f14e6a6311d2152283b52dbcf2633fce3
Mederic <32560642+mederic-p@users.noreply.github.com>
2018-08-24 10:33:45 +0700

    Add signup confirmation view (#16)

---
+++ POSClient/ViewModels/SignupConfirmationViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  SignupConfirmationViewModel.swift
+//  POSClient
+//
+//  Created by Mederic Petit on 23/8/18.
+//  Copyright © 2018 Omise Go Pte. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class SignupConfirmationViewModel: BaseViewModel {
+    let title = "signup_verification.label.title".localized()
+    let hint = "signup_verification.label.hint".localized()
+    let gotItButtonTitle = "signup_verification.button.got_it".localized()
+}
