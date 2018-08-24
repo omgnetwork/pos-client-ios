f33fe42e35c78a78a19192be9fdddefb21caab5f
mederic <mederic@omise.co>
2018-08-24 17:49:15 +0700

    Update back image

---
+++ POSClient/ViewControllers/BaseNavigationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BaseNavigationViewController.swift
+//  POSClient
+//
+//  Created by Mederic Petit on 24/8/18.
+//  Copyright © 2018 Omise Go Pte. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class BaseNavigationViewController: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let backImage = UIImage(named: "back_arrow")
+        self.navigationBar.backIndicatorImage = backImage
+        self.navigationBar.backIndicatorTransitionMaskImage = backImage
+    }
+}
