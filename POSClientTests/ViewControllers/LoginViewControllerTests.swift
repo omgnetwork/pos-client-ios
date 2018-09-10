d5c99805373b4d7c0718b042544ec0b05b3cd424
mederic <mederic@omise.co>
2018-09-10 16:55:21 +0700

    Add missing LoginViewController tests

--- POSClientTests/ViewControllers/LoginViewControllerTests.swift
+++ POSClientTests/ViewControllers/LoginViewControllerTests.swift
@@ -25,6 +25,12 @@ class LoginViewControllerTests: XCTestCase {
         self.sut = nil
     }
 
+    private func mountOnWindow() {
+        let w = UIWindow()
+        w.addSubview(self.sut.view)
+        w.makeKeyAndVisible()
+    }
+
     func testSetupsCorrectly() {
         XCTAssertEqual(self.sut.emailTextField.placeholder, self.sut.viewModel.emailPlaceholder)
         XCTAssertEqual(self.sut.passwordTextField.placeholder, self.sut.viewModel.passwordPlaceholder)
@@ -59,5 +65,52 @@ class LoginViewControllerTests: XCTestCase {
         let error = POSClientError.unexpected
         self.sut.viewModel.onFailedLogin?(error)
         XCTAssertEqual(ToastCenter.default.currentToast!.text, error.message)
+    }
+
+    func testEmailTextFieldChangeUpdatesViewModelEmailAttribute() {
+        let range = NSRange(location: 0, length: 0)
+        _ = self.sut.textField(self.sut.emailTextField, shouldChangeCharactersIn: range, replacementString: "123")
+        XCTAssertEqual(self.sut.viewModel.email, "123")
+    }
+
+    func testPasswordTextFieldChangeUpdatesViewModelPasswordAttribute() {
+        let range = NSRange(location: 0, length: 0)
+        _ = self.sut.textField(self.sut.passwordTextField, shouldChangeCharactersIn: range, replacementString: "123")
+        XCTAssertEqual(self.sut.viewModel.password, "123")
+    }
+
+    func testReturningEmailTFFocusPasswordTF() {
+        self.mountOnWindow()
+        self.sut.emailTextField.becomeFirstResponder()
+        XCTAssertTrue(self.sut.emailTextField.isFirstResponder)
+        _ = self.sut.textFieldShouldReturn(self.sut.emailTextField)
+        let e = self.expectation(description: "Returning email tf focus password tf")
+        dispatchMain { e.fulfill() }
+        self.waitForExpectations(timeout: 1, handler: nil)
+        XCTAssertTrue(self.sut.passwordTextField.isFirstResponder)
+    }
+
+    func testReturningPasswordTFResignFirstResponderAndTriggersLogIn() {
+        self.mountOnWindow()
+        self.sut.passwordTextField.becomeFirstResponder()
+        XCTAssertTrue(self.sut.passwordTextField.isFirstResponder)
+        _ = self.sut.textFieldShouldReturn(self.sut.passwordTextField)
+        let e = self.expectation(description: "Returning password tf triggers log in")
+        dispatchMain { e.fulfill() }
+        self.waitForExpectations(timeout: 1, handler: nil)
+        XCTAssertFalse(self.sut.passwordTextField.isFirstResponder)
+        XCTAssertEqual(ToastCenter.default.currentToast!.text, "Missing required fields")
+    }
+
+    func testClearEmailTFClearsViewModelEmail() {
+        self.sut.viewModel.email = "123"
+        _ = self.sut.textFieldShouldClear(self.sut.emailTextField)
+        XCTAssertEqual(self.sut.viewModel.email, "")
+    }
+
+    func testClearPWTFClearsViewModelPW() {
+        self.sut.viewModel.password = "123"
+        _ = self.sut.textFieldShouldClear(self.sut.passwordTextField)
+        XCTAssertEqual(self.sut.viewModel.password, "")
     }
 }
