c894b90891dee19bd6b92e3d1f11bc4b8a9934fc
mederic <mederic@omise.co>
2018-09-14 12:13:59 +0700

    Fixed failing test

--- POSClientTests/Views/TransactionTableViewCellTests.swift
+++ POSClientTests/Views/TransactionTableViewCellTests.swift
@@ -13,10 +13,11 @@ class TransactionTableViewCellTests: XCTestCase {
     func testSetupsCorrectly() {
         let bundle = Bundle(for: TransactionTableViewCell.self)
         let sut = bundle.loadNibNamed("TransactionTableViewCell", owner: nil)?.first as? TransactionTableViewCell
-        let vm = TransactionCellViewModel(transaction: StubGenerator.transactions().first!, currentUserAddress: "x")
+        let transaction = StubGenerator.transactions().first!
+        let vm = TransactionCellViewModel(transaction: transaction, currentUserAddress: "x")
         sut!.transactionCellViewModel = vm
         XCTAssertEqual(sut!.nameLabel.text, "Starbucks - CDC")
-        XCTAssertEqual(sut!.timestampLabel.text, "Aug 23, 14:19")
+        XCTAssertEqual(sut!.timestampLabel.text, transaction.createdAt.toString(withFormat: "MMM dd, HH:mm"))
         XCTAssertEqual(sut!.amountLabel.text, "+ 10 TK1")
         XCTAssertEqual(sut!.amountLabel.textColor, Color.transactionCreditGreen.uiColor())
         XCTAssertEqual(sut!.statusTextLabel.text, "Success")
