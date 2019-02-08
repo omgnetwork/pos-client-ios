fcb0f72044392aec8cb52ec78d93e75546ebe688
mederic <mederic@omise.co>
2019-02-08 14:26:15 +0700

    Add open settings button to scanner not available view

--- POSClient/ViewControllers/ScannerNotAvailableViewController.swift
+++ POSClient/ViewControllers/ScannerNotAvailableViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ScannerNotAvailableViewController: BaseViewController {
     @IBOutlet var hintLabel: UILabel!
+    @IBOutlet var openSettingsButton: UIButton!
 
     override func configureView() {
         super.configureView()
         self.hintLabel.text = "scanner_not_available.label.hint".localized()
+        self.openSettingsButton.setTitle("scanner_not_available.button.open_settings".localized(), for: .normal)
+        self.openSettingsButton.isHidden = self.settingsURL() == nil
+    }
+
+    @IBAction func tapOpenSettings(_: Any) {
+        guard let settingsUrl = self.settingsURL() else { return }
+        let optionsKeyDictionary = [UIApplication.OpenExternalURLOptionsKey(rawValue: "universalLinksOnly"): NSNumber(value: true)]
+
+        UIApplication.shared.open(settingsUrl, options: optionsKeyDictionary, completionHandler: nil)
+    }
+
+    private func settingsURL() -> URL? {
+        guard let settingsUrl = URL(string: UIApplication.openSettingsURLString), UIApplication.shared.canOpenURL(settingsUrl) else {
+            return nil
+        }
+        return settingsUrl
     }
 }
